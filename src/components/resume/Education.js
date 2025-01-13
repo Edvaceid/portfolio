cf0acd308fff7015f124bc32e7530491b54d398c
Arda Sevik <edvaceid@gmail.com>
2025-01-13 17:11:58 +0300

    ardasevik.13.01.2024

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -12,21 +12,21 @@ const Education = () => {
         <ResumeTitle title="Experience" icon={<MdWork />} />
         <ResumeCard
           badge="2024"
-          title="Flutter Developer / Intern Computer Engineer (Cyber Security Department)"
-          subTitle="Arti ve Arti Technology Services Industry and Trade Inc."
-          des="I was asked to develop a cyber security training/assistance application. I was asked to provide Authentications kept with FireBase Authentication, photos kept secure with FireStore and Storage and data, slides, quizzes, rank system and artificial intelligence integration, making learning fun, but also I offered an application where they can get help from professionals on the platform at any time"
+          title="Mobile Application Developer"
+          subTitle="Eterna Technology Inc. | Mersin Technoscope | Technopark"
+          des="Accompanied by our project managers, I undertook the integration of the developed/developed/to-bedeveloped software into IOS and Android operating systems with WebView and Flutter, publishing in AppStore and Play Store, mobile environment testing and Figma designs of the developed software. Here I experienced real teamwork and professional environment."
+        />
+        <ResumeCard
+          badge="2024"
+          title="Mobile Application Developer - Intern Computer Engineer - Cybersecurity Department"
+          subTitle="Artı ve Artı Technology Services Industry and Trade Inc. | HP & HPE ServiceOne Platinum Partner"
+          des="I was tasked to develop a cyber security training/assistance application. I provided users with authentications kept with FireBase Authentication, photos and data kept safe with FireStore and Storage, slides, tests, rank system and artificial intelligence integration to make learning fun and at the same time they can get help from professionals on the platform at any time."
         />
         <ResumeCard
           badge="2023"
-          title="Intern Computer Engineer - ERP/CRM Development"
-          subTitle="Sim Information Technologies - AKINSOFT Mersin Regional Dealer"
-          des="When I started my internship, it coincided with the time when the company was about to start a project. This project was to develop an ERP (Enterprise Resource Planning) application for a group of managers who had a fleet of vehicles in their company. In the application developed with Flutter, managers and employees use separate login methods, and many things such as vehicle tracking, employee tracking, income and expense tracking, location tracking, requests, etc. can be viewed with a single touch."
-        />
-        <ResumeCard
-          badge="2021 - 2023"
-          title="Barista"
-          subTitle="Chain and Boutique Coffee Companies"
-          des="During my student life, I worked as a barista in many chain coffee companies and boutique cafes in order to make pocket money for myself. For example, Sabiha Gökçen Airport International Department Starbucks, Sayapark Soulmate... I have a total of more than one year of experience as a barista"
+          title="Mobile Application Developer - Intern Computer Engineer - ERP/CRM Development"
+          subTitle="Sim Information Technologies | AKINSOFT Mersin Regional Dealer"
+          des="The period when I started my internship coincided with the time when the company was about to start a project. This project was to develop an ERP (Enterprise Resource Planning) application for a group of managers who had a fleet of vehicles in their company. In the application developed with Flutter, managers and employees use separate login methods, and many things such as vehicle tracking, employee tracking, income and expense tracking, location tracking, requests, etc. can be viewed with a single touch."
         />
       </div>
 
